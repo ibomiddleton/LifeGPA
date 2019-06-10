419969aaf76aeade8b0e3371112cbd47c860176a
Isaac  Middleton <Isaac@Isaacs-MacBook-Pro.local>
2019-06-10 18:43:29 -0400

    Updated dashboard

---
+++ components/Scroll.js
@@ -0,0 +1,21 @@
+(function() {
+    'use strict';
+  
+    var btnScrollDown = document.querySelector('.read-more');
+  
+    function scrollDown() {
+      var windowCoords = document.documentElement.clientHeight;
+      (function scroll() {
+        if (window.pageYOffset < windowCoords) {
+          window.scrollBy(0, 10);
+          setTimeout(scroll, 0);
+        }
+        if (window.pageYOffset > windowCoords) {
+          window.scrollTo(0, windowCoords);
+        }
+      })();
+    }
+  
+    btnScrollDown.addEventListener('click', scrollDown);
+  })();
+
--- components/Tabs.js
+++
@@ -1,2 +0,0 @@
-// This page is for the user to view their percentages
-// They will be able to filter out the days of the week
